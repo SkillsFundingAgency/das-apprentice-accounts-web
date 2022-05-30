80118fd8f614faf3c8a0a31f506d6a08f20c32f1
Sarah <sarah.coetzee@education.gov.uk>
2022-05-30 20:15:10 +0100

    QF-247 Get validation working,optimise html code, Create a new endpoint to batch update apprentice preferences

---
+++ src/SFA.DAS.ApprenticeAccounts.Web/Services/InnerApi/UpdateApprenticePreferencesCommand.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace SFA.DAS.ApprenticeAccounts.Web.Services.InnerApi
+{
+    public class UpdateApprenticePreferencesCommand
+    {
+        public List<UpdateApprenticePreferenceCommand> ApprenticePreferences { get; set; }
+    }
+}
