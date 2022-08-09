448dc7e31c8c9ba2028e72e205f6f7854a671a72
codewolf86 <chris@codewolfconsulting.com>
2022-08-09 11:20:49 +0100

    Update validation helper to be create or merge

--- src/SFA.DAS.ApprenticeAccounts.Web/TagHelpers/ValidationHelpers.cs
+++ src/SFA.DAS.ApprenticeAccounts.Web/TagHelpers/ValidationHelpers.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
@@ -16,12 +17,31 @@ namespace SFA.DAS.ApprenticeAccounts.Web.TagHelpers
         public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
         {
             if (PropertyIsInvalid())
-                output.Attributes.Add("class", "govuk-form-group--error");
+                CreateOrMergeAttribute("class", "govuk-form-group--error", output);
         }
 
         bool PropertyIsInvalid()
         {
             return ViewContext?.ModelState[PropertyName]?.ValidationState == ModelValidationState.Invalid;
+        }
+
+        void CreateOrMergeAttribute(string name, object content, TagHelperOutput output)
+        {
+            var currentAttribute = output.Attributes.FirstOrDefault(attribute => attribute.Name == name);
+            if (currentAttribute == null)
+            {
+                var attribute = new TagHelperAttribute(name, content);
+                output.Attributes.Add(attribute);
+            }
+            else
+            {
+                var newAttribute = new TagHelperAttribute(
+                    name,
+                    $"{currentAttribute.Value.ToString()} {content.ToString()}",
+                    currentAttribute.ValueStyle);
+                output.Attributes.Remove(currentAttribute);
+                output.Attributes.Add(newAttribute);
+            }
         }
     }
 }
