491dc7b8a9fe29d817002847afcbe44bd1fde48a
Sarah Duncan <sarah.coetzee@education.gov.uk>
2023-03-29 12:31:19 +0100

    QF-805 change application name

--- src/SFA.DAS.ApprenticeAccounts.Web/Startup/DataProtectionStartup.cs
+++ src/SFA.DAS.ApprenticeAccounts.Web/Startup/DataProtectionStartup.cs
@@ -17,7 +17,7 @@ namespace SFA.DAS.ApprenticeAccounts.Web.Startup
             {
                 services.AddDistributedMemoryCache();
                 services.AddDataProtection()
-                    .SetApplicationName("portal");
+                    .SetApplicationName("apprentice-portal");
             }
             else if (configuration != null)
             {
@@ -28,7 +28,7 @@ namespace SFA.DAS.ApprenticeAccounts.Web.Startup
                     .Connect($"{redisConnectionString},{dataProtectionKeysDatabase}");
 
                 services.AddDataProtection()
-                    .SetApplicationName("portal")
+                    .SetApplicationName("apprentice-portal")
                     .PersistKeysToStackExchangeRedis(redis, "DataProtection-Keys");
             }
 
