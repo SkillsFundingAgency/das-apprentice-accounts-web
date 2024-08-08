68a3e98b914a2b476e4987e4dd5deac69f6e9ad3
Chris Foster <chrisfoster186@googlemail.com>
2024-08-08 14:32:00 +0100

    HIdes nav bar

--- src/SFA.DAS.ApprenticeAccounts.Web/Pages/ChangeYourSignInDetails.cshtml.cs
+++ src/SFA.DAS.ApprenticeAccounts.Web/Pages/ChangeYourSignInDetails.cshtml.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SFA.DAS.ApprenticePortal.SharedUi.Filters;
+using SFA.DAS.ApprenticePortal.SharedUi.Menu;
 
 namespace SFA.DAS.ApprenticeAccounts.Web.Pages
 {
+    [HideNavigationBar]
     public class ChangeYourSignInDetailsModel : PageModel
     {
         public void OnGet()
