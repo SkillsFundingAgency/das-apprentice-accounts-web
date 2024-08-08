39161eff4c9aa18bf56a78cae0e411b1cad294fd
Chris Foster <chrisfoster186@googlemail.com>
2024-08-08 13:46:05 +0100

    Basic page implementation

---
+++ src/SFA.DAS.ApprenticeAccounts.Web/Pages/ChangeYourSignInDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SFA.DAS.ApprenticeAccounts.Web.Pages
+{
+    public class ChangeYourSignInDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
