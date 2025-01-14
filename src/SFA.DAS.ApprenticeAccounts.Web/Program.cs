34286224d6f36c5cfa713fb9e1da12565e3ff82f
Ali Reid <ali.reid@live.co.uk>
2025-01-14 08:19:05 +0000

    remove kibana

--- src/SFA.DAS.ApprenticeAccounts.Web/Program.cs
+++ src/SFA.DAS.ApprenticeAccounts.Web/Program.cs
@@ -1,6 +1,5 @@
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using NLog.Web;
 using SFA.DAS.ApprenticeAccounts.Web.Startup;
 
 namespace SFA.DAS.ApprenticeAccounts.Web
@@ -9,14 +8,12 @@ namespace SFA.DAS.ApprenticeAccounts.Web
     {
         public static void Main(string[] args)
         {
-            NLogStartup.ConfigureNLog();
             CreateHostBuilder(args).Build().Run();
         }
 
         public static IWebHostBuilder CreateHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .ConfigureAzureTableConfiguration()
-                .UseStartup<ApplicationStartup>()
-                .UseNLog();
+                .UseStartup<ApplicationStartup>();
     }
 }
